76ef20dcea33cd506c6c0ee6e39826312a41310f
j.k.nowak <jakub.k.nowak@protonmail.com>
2022-06-26 13:21:25 +0200

    Line chart of sexes per month

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ Każda akcja powoduje konieczność ,,kosztownego przeliczenia" trwającego 3 se
 """
 
 import time
+from datetime import date
 from typing import Any
 import plotly.express as px  # type: ignore
 import pandas as pd  # type: ignore
@@ -16,6 +17,10 @@ from dash.dependencies import Input, Output  # type: ignore
 
 app = Dash(__name__, assets_folder="../assets")
 
+months_dict = {'styczeń': 1, 'luty': 2, 'marzec': 3,
+               'kwiecień': 4, 'maj': 5, 'czerwiec': 6,
+               'lipiec': 7,'sierpień': 8,'wrzesień': 9,
+               'październik': 10, 'listopad': 11, 'grudzień': 12}
 # źródło danych
 # https://bdl.stat.gov.pl/bdl/dane/podgrup/wymiary
 # https://bdl.stat.gov.pl/bdl/dane/podgrup/wymiary
@@ -25,6 +30,7 @@ with open(
     errors="ignore",
 ) as f:
     df: pd.DataFrame = pd.read_csv(f, sep=";")
+    df['Data'] = df.apply(lambda x: date(x['Rok'], months_dict.get(x['Miesiące']), 1), axis=1)
 '''
 with open(
     "data/raw_data/bezrobocie_wyksz_plec_lata.csv",
@@ -56,20 +62,21 @@ app.layout = html.Div(
             ]
         ),
         html.Br(),
-        # slider
-        html.Div(
-            [
-                html.H3(children="Płeć", className="card"),
-                dcc.Dropdown(
-                    df["Miesiące"].unique(),
-                    df["Miesiące"].unique(),
-                    id="month-selection",
-                    multi=True,
-                ),
-            ]
-        ),
+        # date slider
+        html.Div([
+            html.H3(children="Miesiąc", className="card"),
+            dcc.DatePickerRange(
+                id='month-selection',
+                min_date_allowed=date(2017, 1, 1),
+                max_date_allowed=date(2021, 12, 1),
+                initial_visible_month=date(2017, 1, 1),
+                display_format='YYYY-MM',
+                start_date=date(2017, 1, 1),
+                end_date=date(2021, 12, 1)
+            )
+        ]),
         html.Div(dcc.Graph(id="chart")),
-        html.Div(dash_table.DataTable(id="tbl")),
+        #html.Div(dash_table.DataTable(id="tbl")),
 
         # chart 2
 
@@ -79,16 +86,15 @@ app.layout = html.Div(
 
         # chart 5
 
-    ]
-)
+    ], style={'padding': '20px 20px 20px 20px'})
 
 
 # decorator that enables reactivity
 @app.callback(
-    [Output("chart", "figure"), Output("tbl", "data")],
-    [Input("gender-selection", "value"), Input("month-selection", "value")],
+    Output("chart", "figure"),
+    [Input("gender-selection", "value"), Input("month-selection", "start_date"), Input("month-selection", "end_date")],
 )
-def update_graph_1(selected_gender_value: str, month_selection_value: str) -> Any:
+def update_graph_1(selected_gender_value: str, month_selection_start: str, month_selection_end: str) -> Any:
     """
     Updates the plot according to the selected values
 
@@ -99,32 +105,31 @@ def update_graph_1(selected_gender_value: str, month_selection_value: str) -> An
     tmp = df.loc[  # pylint: disable=E1101
         df.loc[:, "Płeć"].isin(selected_gender_value), :  # pylint: disable=E1101
     ]
-    tmp = df.loc[  # pylint: disable=E1101
-          df.loc[:, "Miesiące"].isin(month_selection_value), :  # pylint: disable=E1101
-          ]
+    tmp = tmp[tmp.loc[:, "Data"] <= date.fromisoformat(month_selection_end)]
+    tmp = tmp[tmp.loc[:, "Data"] >= date.fromisoformat(month_selection_start)]
     tmp = (
-        tmp.groupby("Miesiące")
+        tmp.groupby(["Data", "Płeć"])
         .agg({"Wartosc": sum})
         .reset_index()
     )
     # "dlugie obliczenia"
     time.sleep(3)
 
-    fig = px.bar(
+    fig = px.line(
         tmp,
-        x="Miesiące",
+        x="Data",
         y="Wartosc",
-        color="Miesiące",
-        title="Bezrobocie według wykształcenia",
+        color="Płeć",
+        title="Bezrobocie według płci",
         labels={
-            "Miesiące": "Miesiące",
+            "Data": "Data",
             "Wartosc": "Liczba bezrobotnych",
         },
     )
 
     fig.update_layout(barmode="overlay")
 
-    return fig, tmp.to_dict("rows")
+    return fig
 
 
 if __name__ == "__main__":
