68e0f47109bff385b8ec6c38cdbde51c7a3f6fb9
j.k.nowak <jakub.k.nowak@protonmail.com>
2022-06-26 15:24:29 +0200

    Black formatting

--- src/app.py
+++ src/app.py
@@ -17,10 +17,20 @@ from dash.dependencies import Input, Output  # type: ignore
 
 app = Dash(__name__, assets_folder="../assets")
 
-months_dict = {'styczeń': 1, 'luty': 2, 'marzec': 3,
-               'kwiecień': 4, 'maj': 5, 'czerwiec': 6,
-               'lipiec': 7, 'sierpień': 8, 'wrzesień': 9,
-               'październik': 10, 'listopad': 11, 'grudzień': 12}
+months_dict = {
+    "styczeń": 1,
+    "luty": 2,
+    "marzec": 3,
+    "kwiecień": 4,
+    "maj": 5,
+    "czerwiec": 6,
+    "lipiec": 7,
+    "sierpień": 8,
+    "wrzesień": 9,
+    "październik": 10,
+    "listopad": 11,
+    "grudzień": 12,
+}
 # źródło danych
 # https://bdl.stat.gov.pl/bdl/dane/podgrup/wymiary
 # https://bdl.stat.gov.pl/bdl/dane/podgrup/wymiary
@@ -30,7 +40,9 @@ with open(
     errors="ignore",
 ) as f:
     df: pd.DataFrame = pd.read_csv(f, sep=";")
-    df['Data'] = df.apply(lambda x: date(x['Rok'], months_dict.get(x['Miesiące']), 1), axis=1)
+    df["Data"] = df.apply(
+        lambda x: date(x["Rok"], months_dict.get(x["Miesiące"]), 1), axis=1
+    )
 
 with open(
     "data/raw_data/bezrobocie_wyksz_plec_lata.csv",
@@ -38,7 +50,7 @@ with open(
     errors="ignore",
 ) as f:
     df2: pd.DataFrame = pd.read_csv(f, sep=";")
-df2 = df2.iloc[:20,2:6]
+df2 = df2.iloc[:20, 2:6]
 
 # App layout
 app.layout = html.Div(
@@ -51,18 +63,20 @@ app.layout = html.Div(
         ),
         # chart 1
         # date slider
-        html.Div([
-            html.H3(children="Miesiąc", className="card"),
-            dcc.DatePickerRange(
-                id='month-selection',
-                min_date_allowed=date(2017, 1, 1),
-                max_date_allowed=date(2021, 12, 1),
-                initial_visible_month=date(2017, 1, 1),
-                display_format='YYYY-MM',
-                start_date=date(2017, 1, 1),
-                end_date=date(2021, 12, 1)
-            )
-        ]),
+        html.Div(
+            [
+                html.H3(children="Miesiąc", className="card"),
+                dcc.DatePickerRange(
+                    id="month-selection",
+                    min_date_allowed=date(2017, 1, 1),
+                    max_date_allowed=date(2021, 12, 1),
+                    initial_visible_month=date(2017, 1, 1),
+                    display_format="YYYY-MM",
+                    start_date=date(2017, 1, 1),
+                    end_date=date(2021, 12, 1),
+                ),
+            ]
+        ),
         html.Br(),
         # dropdown plec
         html.Div(
@@ -84,8 +98,8 @@ app.layout = html.Div(
             [
                 html.H3(children="Województwo", className="card"),
                 dcc.Dropdown(
-                    df[df.Nazwa != 'POLSKA']["Nazwa"].unique(),
-                    df[df.Nazwa != 'POLSKA']["Nazwa"].unique(),
+                    df[df.Nazwa != "POLSKA"]["Nazwa"].unique(),
+                    df[df.Nazwa != "POLSKA"]["Nazwa"].unique(),
                     id="voivodship-selection",
                     multi=True,
                 ),
@@ -93,39 +107,45 @@ app.layout = html.Div(
         ),
         html.Div(dcc.Graph(id="chart_woj_mo")),
         html.Div(dcc.Graph(id="chart_woj_mo_rel")),
-        #html.Div(dash_table.DataTable(id="tbl")),
-
+        # html.Div(dash_table.DataTable(id="tbl")),
         # chart 2
-        html.Div([
-            dash_table.DataTable(
-                columns = [{'name': col, 'id': col} for col in df2.columns],
-                data = df2.to_dict('records'),
-                editable=True,
-                #filter_action='native',
-                sort_action='native',
-                page_action='native',
-                page_current=0,
-                page_size=20,
-                column_selectable='multi',
-                row_selectable='multi',
-                row_deletable=True
-            )
-        ])
+        html.Div(
+            [
+                dash_table.DataTable(
+                    columns=[{"name": col, "id": col} for col in df2.columns],
+                    data=df2.to_dict("records"),
+                    editable=True,
+                    # filter_action='native',
+                    sort_action="native",
+                    page_action="native",
+                    page_current=0,
+                    page_size=20,
+                    column_selectable="multi",
+                    row_selectable="multi",
+                    row_deletable=True,
+                )
+            ]
+        )
         # chart 3
-
         # chart 4
-
         # chart 5
-
-    ], style={'padding': '20px 20px 20px 20px'})
+    ],
+    style={"padding": "20px 20px 20px 20px"},
+)
 
 
 # decorator that enables reactivity
 @app.callback(
     [Output("chart_plc_mo", "figure"), Output("chart_plc_mo_rel", "figure")],
-    [Input("gender-selection", "value"), Input("month-selection", "start_date"), Input("month-selection", "end_date")],
+    [
+        Input("gender-selection", "value"),
+        Input("month-selection", "start_date"),
+        Input("month-selection", "end_date"),
+    ],
 )
-def update_graph_plc(selected_gender_value: str, month_selection_start: str, month_selection_end: str) -> Any:
+def update_graph_plc(
+    selected_gender_value: str, month_selection_start: str, month_selection_end: str
+) -> Any:
     """
     Updates the plot according to the selected values
 
@@ -139,14 +159,13 @@ def update_graph_plc(selected_gender_value: str, month_selection_start: str, mon
     ]
     tmp = tmp[tmp.loc[:, "Data"] <= date.fromisoformat(month_selection_end)]
     tmp = tmp[tmp.loc[:, "Data"] >= date.fromisoformat(month_selection_start)]
-    tmp = (
-        tmp.groupby(["Data", "Płeć"])
-        .agg({"Wartosc": sum})
-        .reset_index()
+    tmp = tmp.groupby(["Data", "Płeć"]).agg({"Wartosc": sum}).reset_index()
+    tmp["Wartosc_rel"] = tmp.apply(
+        lambda x: x.Wartosc
+        / int(tmp[(tmp.Płeć == x.Płeć) & (tmp.Data == date(2017, 1, 1))]["Wartosc"])
+        * 100,
+        axis=1,
     )
-    tmp['Wartosc_rel'] = tmp.apply(
-        lambda x: x.Wartosc / int(tmp[(tmp.Płeć == x.Płeć) & (tmp.Data == date(2017, 1, 1))]['Wartosc']) * 100,
-        axis=1)
     # "dlugie obliczenia"
     time.sleep(3)
 
@@ -179,9 +198,15 @@ def update_graph_plc(selected_gender_value: str, month_selection_start: str, mon
 
 @app.callback(
     [Output("chart_woj_mo", "figure"), Output("chart_woj_mo_rel", "figure")],
-    [Input("voivodship-selection", "value"), Input("month-selection", "start_date"), Input("month-selection", "end_date")],
+    [
+        Input("voivodship-selection", "value"),
+        Input("month-selection", "start_date"),
+        Input("month-selection", "end_date"),
+    ],
 )
-def update_graph_plc(selected_voivod_value: str, month_selection_start: str, month_selection_end: str) -> Any:
+def update_graph_plc(
+    selected_voivod_value: str, month_selection_start: str, month_selection_end: str
+) -> Any:
     """
     Updates the plot according to the selected values
 
@@ -195,14 +220,13 @@ def update_graph_plc(selected_voivod_value: str, month_selection_start: str, mon
     ]
     tmp = tmp[tmp.loc[:, "Data"] <= date.fromisoformat(month_selection_end)]
     tmp = tmp[tmp.loc[:, "Data"] >= date.fromisoformat(month_selection_start)]
-    tmp = (
-        tmp.groupby(["Data", "Nazwa"])
-        .agg({"Wartosc": sum})
-        .reset_index()
+    tmp = tmp.groupby(["Data", "Nazwa"]).agg({"Wartosc": sum}).reset_index()
+    tmp["Wartosc_rel"] = tmp.apply(
+        lambda x: x.Wartosc
+        / int(tmp[(tmp.Nazwa == x.Nazwa) & (tmp.Data == date(2017, 1, 1))]["Wartosc"])
+        * 100,
+        axis=1,
     )
-    tmp['Wartosc_rel'] = tmp.apply(
-        lambda x: x.Wartosc / int(tmp[(tmp.Nazwa == x.Nazwa) & (tmp.Data == date(2017, 1, 1))]['Wartosc']) * 100,
-        axis=1)
     # "dlugie obliczenia"
     time.sleep(3)
 
